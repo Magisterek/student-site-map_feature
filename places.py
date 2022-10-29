f36c9960fcbd63963fa2f122ccaadaf1f7360561
Magisterek <46652951+Magisterek@users.noreply.github.com>
2022-10-29 21:55:29 +0200

    MVP /2

--- places.py
+++ places.py
@@ -40,12 +40,19 @@ kategoria_jedzenie = [
     "burger",
     "burgery",
     "burgera",
+    "burgers",
     "sniadanie",
     "czekolada",
     "kebab",
     "cafe",
+    "Ciasto",
     "ciastko",
     "ciasta",
+    "bistro",
+    "kołacza",
+    "kołacz",
+    "pączek",
+    "naleśniki"
 ]
 kategoria_alkohol = [
     "koktajl",
@@ -58,13 +65,17 @@ kategoria_alkohol = [
     "shoty",
     "wódka",
     "wódki",
+    "kieliszek",
+    "bacardi",
 ]
-
-kategoria_dostawa_jedzenia = ["glovo", "uber"]
+kategoria_dostawa_jedzenia = ["glovo", "uber", "dostawa"]
 tablica_slownikow = []
 
 
 appended_jedzenie = []
+appended_alkohol = []
+appended_uslugi = []
+appended_kurier = []
 
 header = "miejsce, kupon"
 print(header)
@@ -75,26 +86,37 @@ for miasto, kod in miasto_kod.items():
     miejsce = soup.find_all("p", class_="card-text")
     kupon = soup.find_all("h4", class_="card-title")
     for x in range(len(miejsce)):
-        place = miejsce[x].get_text().replace(",", "")
-        rabat = kupon[x].get_text().capitalize()
-        czy_znalazlo = False
-        print(place)
-        for word in place: # word okazało sie charem
-            # print(word)
-            word = str(word)[24:]
-            print(word)
-
-            if (czy_znalazlo == False) and (word in kategoria_jedzenie):
-                czy_znalazlo = True
-                # appended_jedzenie.append({str(place): str(rabat)})
-        for word in kupon:
-            word = str(word)[24:]
-            if (czy_znalazlo == False) and (word in kategoria_jedzenie):
-                czy_znalazlo = True
-                # appended_jedzenie.append({str(place): str(rabat)})
+        place = miejsce[x].get_text().replace(",", "").lower()
+        rabat = kupon[x].get_text().lower()
+        is_already_founded = False
+        tytul_oferty = place.split(" ")
+        del tytul_oferty[-1]  # usuń miasto z konca tytułu
+        opis_oferty = rabat.split(" ")
+        if is_already_founded == False:
+            for word in tytul_oferty:  # frytki
+                if word in kategoria_jedzenie:  # jedzenie w tytule
+                    is_already_founded = True
+                    appended_jedzenie.append({place.title(): rabat.capitalize()})
+                elif word in kategoria_alkohol:  # alkohol w tytule
+                    is_already_founded = True
+                    appended_alkohol.append({place.title(): rabat.capitalize()})
+        if is_already_founded == False:
+            for word in opis_oferty:
+                if word in kategoria_jedzenie:  # jedzenie w opisie
+                    is_already_founded = True
+                    appended_jedzenie.append({place.title(): rabat.capitalize()})
+                elif word in kategoria_alkohol:  # alohol w opisie
+                    is_already_founded = True
+                    appended_alkohol.append({place.title(): rabat.capitalize()})
+        if is_already_founded == False:  # usługi
+            is_already_founded = True
+            appended_uslugi.append({place: rabat})
 
         # print(rabat)
         # print(miejsce[x].get_text().replace(",", ""), end=",")
         # print(kupon[x].get_text().capitalize())
 print(appended_jedzenie)
-print("end")
+print("######################################################################")
+print(appended_alkohol)
+print("######################################################################")
+print(appended_uslugi)
