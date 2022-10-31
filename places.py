ac014def17c4c79e6b09431a2a2f8a5a6bba7e6b
Magisterek <46652951+Magisterek@users.noreply.github.com>
2022-10-31 23:43:14 +0100

    Linguistic fixes

--- places.py
+++ places.py
@@ -2,34 +2,26 @@ from requests import get
 from bs4 import BeautifulSoup
 import re
 
-####################################################################################
-# wszystkie miasta wraz z ich kodem do zmiany url
 url = "https://estarter.pl/"
-strona = get(url)
-soup = BeautifulSoup(strona.content, "html.parser")
-miasta = soup.find_all("option")
-# print(miasta) # <optionvalue="/pl/offer/index/id_category/403467/category_name/bialystok">Białystok</option>
-miasto_kod = {}
+# page = get(url)
+soup = BeautifulSoup(get(url).content, "html.parser")
+cities = soup.find_all("option")
+# print(cities) == <optionvalue="/pl/offer/index/id_category/403467/category_name/bialystok">Białystok</option>
 
-# pattern na kod
-kod_pattern = r"\d{6}"
-# pattern na miasto
-miasto_pattern = r"^\w+"
+city_and_id = {}
 
-####################################################################################
-for opcja in miasta:
-    # del <option value="/pl/offer/index/id_category/403467/category_name/ zawsze stała wartość, stąd opcja[64:]
-    kod = re.findall(kod_pattern, str(opcja))
-    miasto = re.findall(miasto_pattern, str(opcja)[64:])
-    # print(kod, miasto)
-    if (
-        miasto and kod
-    ):  # celem tego jest pozbycie sie pustych tablic (pierwszy wiersz i wiersz w połowie)
-        miasto_kod.update({miasto[0]: kod[0]})
-# print(miasto_kod)
+city_and_id_pattern = r"\d{6}"
 
-####################################################################################
-# miasto_kod = {"Poznań" :"403417"}
+city_pattern = r"^\w+"
+
+for city_option in cities:
+    # del <option value="/pl/offer/index/id_category/403467/category_name/ zawsze stała wartość, stąd city[64:]
+    id = re.findall(city_and_id_pattern, str(city_option))
+    city = re.findall(city_pattern, str(city_option)[64:])
+    # check for emptiness 
+    if city: # and id
+        city_and_id.update({city[0]: id[0]})
+
 kategoria_jedzenie = [
     "frytki",
     "frytek",
@@ -54,7 +46,8 @@ kategoria_jedzenie = [
     "kołacza",
     "kołacz",
     "pączek",
-    "naleśniki"
+    "naleśniki",
+    "herbata"
 ]
 kategoria_alkohol = [
     "koktajl",
@@ -64,38 +57,48 @@ kategoria_alkohol = [
     "piwa",
     "karafka",
     "shot",
+    "shotów"
     "shoty",
     "wódka",
     "wódki",
     "kieliszek",
     "bacardi",
+    "drink",
+    "kamikaze"
+]
+kategoria_kurierzy = [
+    "uber",
+    "glovo"
 ]
 kategoria_dostawa_jedzenia = ["glovo", "uber", "dostawa"]
-tablica_slownikow = []
-
 
 appended_jedzenie = []
 appended_alkohol = []
 appended_uslugi = []
-appended_kurier = []
+appended_kurierzy = []
 
-wszystkie_kategorie = ["appended_jedzenie","appended_alkohol","appended_uslugi","appended_kurier"] 
+wszystkie_kategorie = [
+    "appended_jedzenie",
+    "appended_alkohol",
+    "appended_uslugi",
+    "appended_kurierzy",
+]
 
-for miasto, kod in miasto_kod.items():
-    url = f"https://estarter.pl/pl/offer/index/id_category/{kod}/category_name/{miasto}"
-    strona = get(url)
-    soup = BeautifulSoup(strona.content, "html.parser")
+for city, id in city_and_id.items():
+    url = f"https://estarter.pl/pl/offer/index/id_category/{id}/category_name/{city}"
+    # page = get(url)
+    soup = BeautifulSoup(get(url).content, "html.parser")
     miejsce = soup.find_all("p", class_="card-text")
-    kupon = soup.find_all("h4", class_="card-title")
+    coupon = soup.find_all("h4", class_="card-title")
     for x in range(len(miejsce)):
         place = miejsce[x].get_text().replace(",", "").lower()
-        rabat = kupon[x].get_text().lower()
+        rabat = coupon[x].get_text().lower()
         is_already_founded = False
-        tytul_oferty = place.split(" ")
-        del tytul_oferty[-1]  # usuń miasto z konca tytułu
+        coupon_title = place.split(" ")
+        del coupon_title[-1]  # usuń city z konca tytułu
         opis_oferty = rabat.split(" ")
         if is_already_founded == False:
-            for word in tytul_oferty:  # frytki
+            for word in coupon_title:  # frytki
                 if word in kategoria_jedzenie:  # jedzenie w tytule
                     is_already_founded = True
                     appended_jedzenie.append({place.title(): rabat.capitalize()})
@@ -116,30 +119,32 @@ for miasto, kod in miasto_kod.items():
 
         # print(rabat)
         # print(miejsce[x].get_text().replace(",", ""), end=",")
-        # print(kupon[x].get_text().capitalize())
+        # print(coupon[x].get_text().capitalize())
 # print(appended_jedzenie)
 # print("######################################################################")
 # print(appended_alkohol)
 # print("######################################################################")
 # print(appended_uslugi)
+
+#generuj pliki 
 header = "miejsce, kupon\n"
-with open('alohol.csv', 'w', encoding="utf-8") as f:
+with open("alohol.csv", "w", encoding="utf-8") as f:
     f.write(header)
-    for miejsce_kupon in appended_alkohol:
-        for k,v in miejsce_kupon.items():
+    for place_coupon in appended_alkohol:
+        for k, v in place_coupon.items():
             f.write(f"{k},{v}\n")
-with open('jedzenie.csv', 'w', encoding="utf-8") as f:
+with open("jedzenie.csv", "w", encoding="utf-8") as f:
     f.write(header)
-    for miejsce_kupon in appended_jedzenie:
-        for k,v in miejsce_kupon.items():
+    for place_coupon in appended_jedzenie:
+        for k, v in place_coupon.items():
             f.write(f"{k},{v}\n")
-with open('uslugi.csv', 'w', encoding="utf-8") as f:
+with open("uslugi.csv", "w", encoding="utf-8") as f:
     f.write(header)
-    for miejsce_kupon in appended_uslugi:
-        for k,v in miejsce_kupon.items():
+    for place_coupon in appended_uslugi:
+        for k, v in place_coupon.items():
             f.write(f"{k},{v}\n")
 
 # for kategoria in wszystkie_kategorie:
-# for miejsce_kupon in appended_alkohol:
-#     for k,v in miejsce_kupon.items():
+# for place_coupon in appended_alkohol:
+#     for k,v in place_coupon.items():
 #         print(k,v)
